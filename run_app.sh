0ca104f63c74301744143842844c8c16894fc1f0
Spencer Krum <nibz@spencerkrum.com>
2018-03-18 15:49:24 -0500

    Further improvements to run script

--- run_app.sh
+++ run_app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker run  -d -it --rm -v $(pwd)/twitch-chat/config.json:/usr/src/app/config.json  -p 3000:3000 twitch-chat:latest
-docker run -d -it --rm -v $(pwd)/csgo-poker/config.yaml:/usr/src/app/config.yaml csgo-poker:latest
+echo "Warning: destroying currently running contianers"
+docker kill csgo-poker twitch-chat
+docker run  -d -it --rm --name twitch-chat -v $(pwd)/twitch-chat/config.json:/usr/src/app/config.json  -p 3000:3000 twitch-chat:latest
+docker run -d -it --rm --name csgo-poker --link twitch-chat:twitch-chat -v $(pwd)/csgo-poker/config.yaml:/usr/src/app/config.yaml csgo-poker:latest
 
 
