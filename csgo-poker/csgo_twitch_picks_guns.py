c7babae76fe8e2409dd84764915bab2626c51dc1
Spencer Krum <nibz@spencerkrum.com>
2018-03-18 15:49:19 -0500

    Set csgo-poker to source from twitch-chat app

--- csgo-poker/csgo_twitch_picks_guns.py
+++ csgo-poker/csgo_twitch_picks_guns.py
@@ -2,9 +2,11 @@ import sys
 import telnetlib
 from time import sleep
 import yaml
+import requests
+import random
 
 config = yaml.safe_load(open('config.yaml'))
-weapons = config['available_weapons']
+available_weapons = config['available_weapons']
 HOST = config['host']
 PORT = config['port']
 welcome = "CSGO Remote Console Online"
@@ -20,6 +22,22 @@ def run(command):
     sleep(1.0)
 
 
+def choose_weapon():
+    r = requests.get(url='http://twitch-chat:3000/words')
+    weapons = r.json()
+    if len(weapons) == 0:
+        return "buy " + random.choice(available_weapons)
+    words = []
+    # shamelessly stolen from:
+    # https://www.saltycrane.com/blog/2007/09/how-to-sort-python-dictionary-by-keys/
+    for key, value in sorted(weapons.iteritems(), key=lambda (k,v): (v,k)):
+        print "%s: %s" % (key, value)
+        words.append(key)
+
+    result = "buy {0}".format(words[-1])
+    return result
+
+
 # Initialize csgo telnet connection
 tn = telnetlib.Telnet(HOST, PORT)
 tn.write("echo " + welcome + endl)
@@ -28,8 +46,10 @@ print("Successfully Connected")
 
 # Loop, waiting for signal that round has restarted
 # Example: 0:  Reinitialized 4 predictable entities
+# Note: only seems to happen when I die, not when I live
 while True:
     tn.read_until("Reinitialized ")
     print "Round start"
     sleep(1.0)
-    run("buy ak47")
+    command = choose_weapon()
+    run(command)
