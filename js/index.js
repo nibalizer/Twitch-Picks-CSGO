979ba2e4fb1a7ab07752c11ff9e87138c5e6cda6
Spencer Krum <nibz@spencerkrum.com>
2018-03-18 01:20:23 -0500

    Add word_count endpoint
    This keeps an object around with a map from words to word counts. The
    first word in a message is all that is considered. Every config.period
    seconds, the object is flushed to keep the data recent.

--- js/index.js
+++ js/index.js
@@ -9,6 +9,11 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+
+var word_count = {};
+var word_count_time = new Date();
+//t.setSeconds(t.getSeconds() + 10);
+
 // Set up options for connection to twitch chat
 // Add channels in the config.json file
 var tmi_options = {
@@ -36,10 +41,26 @@ client.on('chat', function(channel, user, message, self) {
   fs.appendFile('twitch_chat_log_' + channel + '.txt', log_line + "\n", function (err) {
     if (err) throw err;
   });
+  first_word = message.split(' ')[0];
+  if (word_count[first_word] == undefined) {
+    word_count[first_word] = 1;
+  } else {
+    word_count[first_word] += 1;
+  }
+  // Reset word count object every 'period' seconds
+  current_time = new Date();
+  if (current_time - word_count_time > (config.period * 1000)) {
+    word_count = {};
+    word_count_time = new Date();
+  }
 });
 
 app.get('/', function(req, res){
   res.send("Hello World");
+});
+
+app.get('/words', function(req, res){
+  res.json(word_count);
 });
 
 http.listen(3000, function(){
--- js/readme.md
+++ js/readme.md
@@ -5,6 +5,11 @@ It exports the 'selected weapon' over http and the python code calls into it.
 
 Endpoints:
 
-/weapons
+/words
 
-Provides sorted json list of weapons and votes for that weapon.
+Provides sorted json list of words and votes for that word.
+
+
+Parameters: 
+
+See config.json. Period refers to how many seconds between reseting the counters.
