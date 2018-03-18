c3ecbae7b1e3ae630f297a8fd91bc3dbb6f049c8
Spencer Krum <nibz@spencerkrum.com>
2018-03-18 00:04:03 -0500

    Add basic 'buy ak47' in loop code

---
+++ csgo_twitch_picks_guns.py
@@ -0,0 +1,35 @@
+import sys
+import telnetlib
+from time import sleep
+import yaml
+
+HOST = "192.168.0.2"
+PORT = 2121
+welcome = "CSGO Remote Console Online"
+endl = "\n"
+
+config = yaml.safe_load(open('config.yaml'))
+weapons = config['available_weapons']
+
+# Runs commands on the csgo console
+# Sleeps after for rate limiting but does not block
+def run(command):
+    print "running: ", command
+    tn.write("echo Remote Command: " + command + endl)
+    tn.write(command + endl)
+    sleep(1.0)
+
+
+# Initialize csgo telnet connection
+tn = telnetlib.Telnet(HOST, PORT)
+tn.write("echo " + welcome + endl)
+tn.read_until("Online")
+print("Successfully Connected")
+
+# Loop, waiting for signal that round has restarted
+# Example: 0:  Reinitialized 4 predictable entities
+while True:
+    tn.read_until("Reinitialized ")
+    print "Round start"
+    sleep(1.0)
+    run("buy ak47")
