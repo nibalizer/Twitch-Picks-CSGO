bdb20076cd618e481ca63c4d83366144180c72fe
Spencer Krum <nibz@spencerkrum.com>
2018-03-18 14:23:10 -0500

    Dockerize csgo-pocker
    Also provide some small updates to the build/run scripts. Now we're
    forking both docker run's to the background.
    
    View (tail) logs with e.g. 'docker ps; docker logs -f 8a88a8ca8as'

--- build_app.sh
+++ build_app.sh
@@ -3,3 +3,7 @@
 pushd twitch-chat
 docker build -t twitch-chat .
 popd
+
+pushd csgo-poker
+docker build -t csgo-poker .
+popd
--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker run -it --rm -v $(pwd)/twitch-chat/config.json:/usr/src/app/config.json  -p 3000:3000 twitch-chat:latest
+docker run  -d -it --rm -v $(pwd)/twitch-chat/config.json:/usr/src/app/config.json  -p 3000:3000 twitch-chat:latest
+docker run -d -it --rm -v $(pwd)/csgo-poker/config.yaml:/usr/src/app/config.yaml csgo-poker:latest
+
 
